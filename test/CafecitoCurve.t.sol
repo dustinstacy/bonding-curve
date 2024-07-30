8f67f6b55eca3ec05fb9150e657d88fb7487f609
Dustin Stacy <dustin.stacy@outlook.com>
2024-07-30 11:48:23 -0500

    remove unnecessary tests

--- test/CafecitoCurve.t.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-import {Test, console} from "forge-std/Test.sol";
-import {CafecitoCurve} from "src/references/CafecitoCurve.sol";
-import {MockV3Aggregator} from "test/mocks/MockV3Aggregator.sol";
-import {Calculations} from "src/libraries/Calculations.sol";
-
-contract CafecitoCurveTest is Test {}
