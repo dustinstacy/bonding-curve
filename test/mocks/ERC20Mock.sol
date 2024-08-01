b8667d13d6f74250efe877f5caf7bc1af6d24d00
Dustin Stacy <dustin.stacy@outlook.com>
2024-08-01 12:17:44 -0500

    update mocks

--- test/mocks/ERC20Mock.sol
+++ test/mocks/ERC20Mock.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.0;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// mock class using ERC20
+/// @title ERC20Mock
+/// @notice This contract is used for testing purposes only.
 contract ERC20Mock is ERC20 {
     constructor(string memory name, string memory symbol, address initialAccount, uint256 initialBalance)
         payable
