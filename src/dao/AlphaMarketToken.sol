211477ed3b9d6ca1811f976a6988cac070ddc050
Dustin Stacy <dustin.stacy@outlook.com>
2024-08-18 13:58:54 -0500

    add base DAO contracts

---
+++ src/dao/AlphaMarketToken.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20Permit, Nonces} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract AlphaMarketToken is ERC20, ERC20Burnable, Ownable, ERC20Permit, ERC20Votes {
+    constructor(address initialOwner)
+        ERC20("AlphaMarketToken", "ALF")
+        Ownable(initialOwner)
+        ERC20Permit("AlphaMarketToken")
+    {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner) public view override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
+    }
+}
---
+++ src/dao/TimeLock.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract TimeLock is TimelockController {
+    /// @param minDelay Minimum delay for timelock
+    /// @param proposers List of addresses that can propose a transaction
+    /// @param executors List of addresses that can execute a transaction
+    constructor(uint256 minDelay, address[] memory proposers, address[] memory executors)
+        TimelockController(minDelay, proposers, executors, msg.sender)
+    {}
+}
