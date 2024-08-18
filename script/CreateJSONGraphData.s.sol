d8c6b30948c3f3e12797cab911462c0d378da0fb
Dustin Stacy <dustin.stacy@outlook.com>
2024-08-18 18:11:24 -0500

    fix build errors

--- script/CreateJSONGraphData.s.sol
+++ script/CreateJSONGraphData.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity 0.8.26;
 
 import {Script, console2} from "forge-std/Script.sol";
 import {LinearBondingCurve} from "src/linear-curve/LinearBondingCurve.sol";
