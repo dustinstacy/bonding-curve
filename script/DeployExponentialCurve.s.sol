bb32b2912753a669fe9d29c653efc392a535e629
Dustin Stacy <dustin.stacy@outlook.com>
2024-08-19 15:28:04 -0500

    update curve deploy scripts

--- script/DeployExponentialCurve.s.sol
+++ script/DeployExponentialCurve.s.sol
@@ -3,25 +3,42 @@ pragma solidity ^0.8.26;
 
 import {ExponentialBondingCurve} from "src/exponential-curve/ExponentialBondingCurve.sol";
 import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "script/HelperConfig.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 /// @title DeployExponentialBondingCurve
 /// @notice Script for deploying the ExponentialBondingCurve contract.
 contract DeployExponentialBondingCurve is Script {
-    /// @return proxy The address of the deployed ExponentialBondingCurve proxy.
-    function run() external returns (address proxy) {
-        return proxy = deployCurve();
+    function run() external {
+        deployCurve();
     }
 
     /// @notice Deploys the ExponentialBondingCurve contract and sets it up as a proxy.
-    /// @return The address of the deployed ExponentialBondingCurve proxy.
-    function deployCurve() public returns (address) {
+    /// @notice Deploys the HelperConfig contract and encodes the parameters for the ExponentialBondingCurve contract.
+    /// @return proxy The address of the deployed ExponentialBondingCurve proxy.
+    /// @return helperConfig The address of the deployed HelperConfig contract.
+    function deployCurve() public returns (address proxy, HelperConfig helperConfig) {
+        helperConfig = new HelperConfig();
+        HelperConfig.CurveConfig memory config = helperConfig.getConfig();
+
+        bytes memory data = abi.encodeWithSelector(
+            ExponentialBondingCurve.initialize.selector,
+            config.owner,
+            config.protocolFeeDestination,
+            config.protocolFeePercent,
+            config.feeSharePercent,
+            config.initialReserve,
+            config.reserveRatio,
+            config.maxGasLimit
+        );
+
         vm.startBroadcast();
         // Deploy the ExponentialBondingCurve contract.
         ExponentialBondingCurve bondingCurve = new ExponentialBondingCurve();
         // Deploy the ERC1967Proxy contract and set the ExponentialBondingCurve contract as the implementation.
-        ERC1967Proxy proxy = new ERC1967Proxy(address(bondingCurve), "");
+        ERC1967Proxy proxyContract = new ERC1967Proxy(address(bondingCurve), data);
+        proxy = address(proxyContract);
         vm.stopBroadcast();
-        return address(proxy);
+        return (proxy, helperConfig);
     }
 }
--- script/DeployLinearBondingCurve.s.sol
+++ script/DeployLinearBondingCurve.s.sol
@@ -3,25 +3,41 @@ pragma solidity ^0.8.26;
 
 import {LinearBondingCurve} from "src/linear-curve/LinearBondingCurve.sol";
 import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "script/HelperConfig.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 /// @title DeployLinearBondingCurve
 /// @notice Script for deploying the LinearBondingCurve contract.
 contract DeployLinearBondingCurve is Script {
-    /// @return proxy The address of the deployed LinearBondingCurve proxy.
-    function run() external returns (address proxy) {
-        return proxy = deployCurve();
+    function run() external {
+        deployCurve();
     }
 
     /// @notice Deploys the LinearBondingCurve contract and sets it up as a proxy.
-    /// @return The address of the deployed LinearBondingCurve proxy.
-    function deployCurve() public returns (address) {
+    /// @notice Deploys the HelperConfig contract and encodes the parameters for the LinearBondingCurve contract.
+    /// @return proxy The address of the deployed LinearBondingCurve proxy.
+    /// @return helperConfig The address of the deployed HelperConfig contract.
+    function deployCurve() public returns (address proxy, HelperConfig helperConfig) {
+        helperConfig = new HelperConfig();
+        HelperConfig.CurveConfig memory config = helperConfig.getConfig();
+
+        bytes memory data = abi.encodeWithSelector(
+            LinearBondingCurve.initialize.selector,
+            config.owner,
+            config.protocolFeeDestination,
+            config.protocolFeePercent,
+            config.feeSharePercent,
+            config.initialReserve,
+            config.maxGasLimit
+        );
+
         vm.startBroadcast();
-        // Deploy the ExponentialBondingCurve contract.
+        // Deploy the LinearBondingCurve contract.
         LinearBondingCurve bondingCurve = new LinearBondingCurve();
         // Deploy the ERC1967Proxy contract and set the LinearBondingCurve contract as the implementation.
-        ERC1967Proxy proxy = new ERC1967Proxy(address(bondingCurve), "");
+        ERC1967Proxy proxyContract = new ERC1967Proxy(address(bondingCurve), data);
+        proxy = address(proxyContract);
         vm.stopBroadcast();
-        return address(proxy);
+        return (proxy, helperConfig);
     }
 }
