7fdaadd5a80035d8c83e013354c92bbe9b9bcf35
Dustin Stacy <dustin.stacy@outlook.com>
2024-09-24 13:57:18 -0500

    add 's

--- src/group/GroupCampaigns.sol
+++ src/group/GroupCampaigns.sol
@@ -18,6 +18,12 @@ contract GroupCampaigns {
         bool active;
     }
 
+    struct Sponsor {
+        address sponsor;
+        uint256 amount;
+        bool accepted;
+    }
+
     /// @notice The total number of campaigns created.
     uint256 public campaignCount;
 
@@ -32,6 +38,18 @@ contract GroupCampaigns {
 
     /// @notice A mapping of campaigns by ID.
     mapping(uint256 campaignId => Campaign) public campaignById;
+
+    /// @notice A mapping of sponsor requests by campaign.
+    mapping(uint256 campaignId => address[]) public campaignSponsorRequests;
+
+    /// @notice A mapping of sponsors by campaign.
+    mapping(uint256 campaignId => Sponsor[]) public campaignSponsors;
+
+    /// @notice A mapping of sponsor request funds by campaign.
+    mapping(uint256 campaignId => uint256 pendingFunds) public campaignPendingFunds;
+
+    /// @notice A mapping of campaign balances.
+    mapping(uint256 campaignId => uint256 balance) public campaignBalances;
 
     /*///////////////////////////////////////////////////////////////
                                 EVENTS
@@ -81,6 +99,76 @@ contract GroupCampaigns {
         campaignCount++;
     }
 
+    /// @notice Allows a user to request to sponsor a campaign.
+    /// @param campaignId The ID of the campaign.
+    /// @param sponsor The address of the sponsor.
+    function requestToSponsor(uint256 campaignId, address sponsor) public payable {
+        Campaign storage campaign = campaignById[campaignId];
+        if (block.timestamp > campaign.deadline) {
+            // Campaign is over
+        }
+        else if (campaign.slotsAvailable == 0) {
+            // No slots available
+        }
+        else if (msg.value < campaign.slotPrice) {
+            // Not enough funds
+        }
+        else {
+            campaign.slotsAvailable--;
+            campaignPendingFunds[campaignId] += campaign.slotPrice;
+            campaignSponsorRequests[campaignId].push(sponsor);
+        }
+    }
+
+    /// @notice Allows a group host to accept a sponsor for a campaign.
+    /// @param campaignId The ID of the campaign.
+    /// @param sponsor The address of the sponsor.
+    function acceptSponsor(uint256 campaignId, address sponsor) public {
+        if (campaignSponsorRequests[campaignId].length == 0) {
+            // No sponsors to accept
+        }
+        else {
+            campaignSponsors[campaignId].push(Sponsor(sponsor, campaignById[campaignId].slotPrice, true));
+            campaignBalances[campaignId] += campaignById[campaignId].slotPrice;
+            campaignPendingFunds[campaignId] -= campaignById[campaignId].slotPrice;
+        }
+    }
+
+    /// @notice Allows a group host to deny a sponsor for a campaign.
+    /// @param campaignId The ID of the campaign.
+    /// @param sponsor The address of the sponsor.
+    function denySponsor(uint256 campaignId, address sponsor) public {
+        uint256 length = campaignSponsorRequests[campaignId].length;
+        if (length == 0) {
+            // No sponsors to deny
+        }
+
+        /// @dev Start with a max value to indicate not found
+        uint256 index = type(uint256).max;
+
+        // Find the index of the sponsor to remove
+        for (uint256 i = 0; i < length; i++) {
+            if (campaignSponsorRequests[campaignId][i] == sponsor) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index > length) {
+            // Sponsor not found
+        }
+
+        // Swap with the last element and then pop
+        campaignSponsorRequests[campaignId][index] = campaignSponsorRequests[campaignId][length - 1];
+        campaignSponsorRequests[campaignId].pop();
+
+        campaignPendingFunds[campaignId] -= campaignById[campaignId].slotPrice;
+        (bool success,) = sponsor.call{value: campaignById[campaignId].slotPrice}("");
+        if (!success) {
+            // Failed to send funds back to sponsor
+        }
+    }
+
     /*///////////////////////////////////////////////////////////////
                           GETTER FUNCTIONS
     ///////////////////////////////////////////////////////////////*/
@@ -95,5 +183,29 @@ contract GroupCampaigns {
     /// @param group The address of the group.
     function getGroupCampaigns(address group) public view returns (Campaign[] memory) {
         return campaignsByGroup[group];
+    }
+
+    /// @notice Gets the sponsor requests for a campaign.
+    /// @param campaignId The ID of the campaign.
+    function getCampaignSponsorRequests(uint256 campaignId) public view returns (address[] memory) {
+        return campaignSponsorRequests[campaignId];
+    }
+
+    /// @notice Gets the sponsors for a campaign.
+    /// @param campaignId The ID of the campaign.
+    function getCampaignSponsors(uint256 campaignId) public view returns (Sponsor[] memory) {
+        return campaignSponsors[campaignId];
+    }
+
+    /// @notice Gets the pending funds for a campaign.
+    /// @param campaignId The ID of the campaign.
+    function getCampaignPendingFunds(uint256 campaignId) public view returns (uint256) {
+        return campaignPendingFunds[campaignId];
+    }
+
+    /// @notice Gets the campaign balance.
+    /// @param campaignId The ID of the campaign.
+    function getCampaignBalance(uint256 campaignId) public view returns (uint256) {
+        return campaignBalances[campaignId];
     }
 }
